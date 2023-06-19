b22125c3fdc1759a375e0a255f78cdb5fde8361b
dashachernyh <55274971+dashachernyh@users.noreply.github.com>
2023-06-19 13:13:55 +0300

    Update table_inform.h

--- table_inform.h
+++ table_inform.h
@@ -4,12 +4,14 @@
 struct table_inform {
     std::string name;
     int table;
+    int income;
     Time start;
     Time count;
     bool empty;
 
     static int count_free;
 
-    table_inform() : table(-1), start("00:00"), count("00:00"), empty(true) {}
+    table_inform() : table(-1), income(0), start("00:00"),
+    count("00:00"), empty(true) {}
 };
 int table_inform::count_free = 0;
